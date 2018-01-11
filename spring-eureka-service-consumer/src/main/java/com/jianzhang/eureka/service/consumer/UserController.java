7a9e14605e2dab472d3a89eeeae8ea45c9343d84
JianJang2017 <anyofus4135@qq.com>
2018-01-11 18:29:00 +0800

    -post请求参数获取为空bug修复（post请求参数通body传输参数）

--- spring-eureka-service-consumer/src/main/java/com/jianzhang/eureka/service/consumer/UserController.java
+++ spring-eureka-service-consumer/src/main/java/com/jianzhang/eureka/service/consumer/UserController.java
@@ -7,6 +7,9 @@ import com.jianzhang.cloud.message.UserResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,10 +39,11 @@ public class UserController {
 	@GetMapping("/user")
 	public User getUserById(Long id) {
 		log.info("GET_USER_URL:{}", GET_USER_URL);
-		//方法1：
-		//ResponseEntity<User> entity= restTemplate.getForEntity(GET_USER_URL + "?id={1}", User.class, id);
-		//User user =entity.getBody();
-		User user = restTemplate.getForObject(GET_USER_URL + "?id={1}", User.class, id);
+		// 方法1：
+		// ResponseEntity<User> entity= restTemplate.getForEntity(GET_USER_URL +
+		// "?id={1}", User.class, id);
+		// User user =entity.getBody();
+		User user = restTemplate.getForObject(GET_USER_URL + "?id={id}", User.class, id);
 		log.info("response User:{}", user.toString());
 		return user;
 
@@ -49,12 +53,13 @@ public class UserController {
 	public UserResponse getUserList(UserRequest request) {
 		log.info("GET_USER_LIST_URL:{}", GET_USER_LIST_URL);
 		Map<String, String> params = Maps.newHashMap();
-		params.put("id", request.getId()+"");
+		params.put("id", request.getId() + "");
 		params.put("name", request.getName());
-		//方法1：
-		//ResponseEntity<UserResponse> entity restTemplate.getForEntity(GET_USER_LIST_URL
-		//		+ "?id={id}&name={name}", UserResponse.class, params);
-		//UserResponse userResponse=entity.getBody();
+		// 方法1：
+		// ResponseEntity<UserResponse> entity
+		// restTemplate.getForEntity(GET_USER_LIST_URL
+		// + "?id={id}&name={name}", UserResponse.class, params);
+		// UserResponse userResponse=entity.getBody();
 		UserResponse userResponse = restTemplate.getForObject(GET_USER_LIST_URL
 				+ "?id={id}&name={name}", UserResponse.class, params);
 		log.info("response User:{}", userResponse.toString());
@@ -65,9 +70,14 @@ public class UserController {
 	@PostMapping("/postUserById")
 	public User postUserById(Long id) {
 		log.info("GET_USER_URL:{}", POST_USER_URL);
-		ResponseEntity<User> entity = restTemplate.postForEntity(POST_USER_URL, id, User.class);
-		//方法2：
-		//User user = restTemplate.postForObject(POST_USER_URL, id, User.class);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Accept", MediaType.APPLICATION_JSON_UTF8_VALUE);
+		headers.setContentType(MediaType.parseMediaType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+		HttpEntity<Long> httpEntity= new HttpEntity<>(id,headers);
+		ResponseEntity<User> entity = restTemplate.postForEntity(POST_USER_URL, httpEntity, User.class);
+		// 方法2：
+		// User user = restTemplate.postForObject(POST_USER_URL, httpEntity,
+		// User.class);
 		log.info("response User:{}", entity.getBody().toString());
 		return entity.getBody();
 
@@ -75,17 +85,21 @@ public class UserController {
 
 	@PostMapping("/postUserList")
 	public UserResponse postUserList(UserRequest request) {
+		log.info("request:{}",request);
 		log.info("GET_USER_LIST_URL:{}", POST_USER_LIST_URL);
-		Map<String, Object> params = Maps.newHashMap();
-		params.put("id", request.getId());
-		params.put("name", request.getName());
-		//可以直接用request，或HttpEntity
-		ResponseEntity<UserResponse> entity = restTemplate.postForEntity(POST_USER_LIST_URL,
-				params, UserResponse.class);
-		//方法2：
-		//UserResponse userResponse =restTemplate.postForObject(POST_USER_LIST_URL,params,UserResponse.class);
-		log.info("response User:{}", entity.getBody().toString());
-		return entity.getBody();
+		HttpHeaders headers = new HttpHeaders();
+		MediaType type = MediaType.parseMediaType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+		headers.setContentType(type);
+		headers.add("Accept", MediaType.APPLICATION_JSON_UTF8_VALUE);
+		HttpEntity<UserRequest> httpEntity = new HttpEntity<>(request, headers);
 
+		// 方法1：使用postForEntity
+		 ResponseEntity<UserResponse> entity =restTemplate.postForEntity(POST_USER_LIST_URL,httpEntity, UserResponse.class);
+		// 方法2：postForObject
+		/*UserResponse userResponse = restTemplate.postForObject(POST_USER_LIST_URL, httpEntity,
+				UserResponse.class);*/
+		//log.info("response User:{}", userResponse);
+		log.info("response User:{}", entity.getBody());
+		return entity.getBody();
 	}
 }
